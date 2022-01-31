c1547e4724f14bf46b55cbd56807f84f4cbe0ad3
Lucas Leonardo <lucas.leonardo@g.globo>
2022-01-31 20:39:20 -0300

    Create test to recursive Sum

--- test/sum_list_test.exs
+++ test/sum_list_test.exs
@@ -1,8 +1,21 @@
 defmodule SumListTest do
   use ExUnit.Case
-  doctest SumList
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "call/1" do
+    test "returns the empty list sum" do
+      list = []
+      response = SumList.call(list)
+      expected_response = 0
+
+      assert response == expected_response
+    end
+
+    test "returns the list sum" do
+      list = [1, 2, 3]
+      response = SumList.call(list)
+      expected_response = 6
+
+      assert response == expected_response
+    end
   end
 end
