8dacfa010adfab56ad4f0e05ecce6440ff9aabd9
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-04 21:42:57 -0300

    Implement a filter function

--- lib/mylist.ex
+++ lib/mylist.ex
@@ -8,9 +8,19 @@ defmodule MyList do
     sum(tail, acc)
   end
 
-  def map([], _), do: []
+  def map([], _func), do: []
 
   def map([hd | tl], func) do
     [func.(hd) | map(tl, func)]
+  end
+
+  def filter([], _func), do: []
+
+  def filter([hd | tl], func) do
+    if func.(hd) do
+      [hd] ++ filter(tl, func)
+    else
+      filter(tl, func)
+    end
   end
 end
--- test/mylist_test.exs
+++ test/mylist_test.exs
@@ -2,17 +2,21 @@ defmodule ListTest do
   use ExUnit.Case
 
   describe "sum_call/1" do
-    test "returns the empty list sum" do
+    test "when empty list is provided, returns zero" do
       list = []
+
       response = MyList.sum_call(list)
+
       expected_response = 0
 
       assert response == expected_response
     end
 
-    test "returns the list sum" do
+    test "when valid list is provided, returns the list sum" do
       list = [1, 2, 3]
+
       response = MyList.sum_call(list)
+
       expected_response = 6
 
       assert response == expected_response
@@ -20,20 +24,48 @@ defmodule ListTest do
   end
 
   describe "map/2" do
-    test "returns the empty list map" do
+    test "when empty list is provided, returns an empty list" do
       list = []
       func = fn x -> x end
+
       response = MyList.map(list, func)
+
       expected_response = []
 
       assert response == expected_response
     end
 
-    test "returns the map list" do
+    test "when valid list is provided, returns the map list" do
       list = [1, 2, 3, 4]
       func = fn x -> x + 1 end
+
       response = MyList.map(list, func)
+
       expected_response = [2, 3, 4, 5]
+
+      assert response == expected_response
+    end
+  end
+
+  describe "filter/2" do
+    test "when empty list is provided, returns an empty list" do
+      list = []
+      func = fn x -> x end
+
+      response = MyList.filter(list, func)
+
+      expected_response = []
+
+      assert response == expected_response
+    end
+
+    test "when valid list is provided, returns the list filtered" do
+      list = [2, 3, 4, 5]
+      func = fn x -> x > 3 end
+
+      response = MyList.filter(list, func)
+
+      expected_response = [4, 5]
 
       assert response == expected_response
     end
